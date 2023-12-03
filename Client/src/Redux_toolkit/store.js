c8419ed16543f674be269aa1449a3537e97f5532
ovi <ataurovishek@gmail.com>
2023-12-04 01:02:53 +0600

    implemented redux-persist

--- Client/src/Redux_toolkit/store.js
+++ Client/src/Redux_toolkit/store.js
@@ -1,14 +1,28 @@
 /* eslint-disable no-unused-vars */
 
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './userSlice';
+import { persistReducer,persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
+
+
+const rootReducer = combineReducers({ user: userSlice.reducer })
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const ReduxStore = configureStore({
-    reducer: {
-        user: userSlice.reducer
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false, // <- add this option
     })
 })
+
+
+export  const PersistStore = persistStore(ReduxStore)
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ReduxStore } from './Redux_toolkit/store.js'
+import { PersistStore, ReduxStore } from './Redux_toolkit/store.js'
 import './index.css'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ReduxStore} >
-      <App />
+      <PersistGate persistor={PersistStore} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )

1a9abfa02864f4b3f98cbc8469f714f83267221e
ovi <ataurovishek@gmail.com>
2023-12-07 15:34:48 +0600

    Revert "implemented redux-persist"
    This reverts commit c8419ed16543f674be269aa1449a3537e97f5532.

--- Client/src/Redux_toolkit/store.js
+++ Client/src/Redux_toolkit/store.js
@@ -1,28 +1,14 @@
 /* eslint-disable no-unused-vars */
 
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './userSlice';
-import { persistReducer,persistStore } from 'redux-persist'
-import storage from 'redux-persist/lib/storage';
 
-
-
-const rootReducer = combineReducers({ user: userSlice.reducer })
-
-const persistConfig = {
-    key: "root",
-    version: 1,
-    storage
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const ReduxStore = configureStore({
-    reducer: persistedReducer,
+    reducer: {
+        user: userSlice.reducer
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false, // <- add this option
     })
 })
-
-
-export  const PersistStore = persistStore(ReduxStore)
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { PersistStore, ReduxStore } from './Redux_toolkit/store.js'
+import { ReduxStore } from './Redux_toolkit/store.js'
 import './index.css'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ReduxStore} >
-      <PersistGate persistor={PersistStore} loading={null}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
 )

a61314c838226154f1554ec4716b7470e86a47e5
ovi <ataurovishek@gmail.com>
2023-12-11 17:14:03 +0600

    created updated ui

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,8 +1,16 @@
+import { useSelector } from "react-redux";
 
 const About = () => {
+
+    const {currentUser} = useSelector(state=>state.user)
+
     return (
-        <div>
-            aabout
+        <div className="mt-10 text-center">
+            <h1 className="text-[1.5vw]">About <span className="text-[blue] text-lg uppercase">{currentUser.username}</span></h1>
+           <img src={currentUser.profilePicture} alt="" className=' rounded-full mx-auto mt-5'/>
+           <p className="text-[2vw] w-[60%] text-center mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque consequuntur quasi fugit a aut debitis incidunt cupiditate dolorem rerum facere!
+           </p>
         </div>
     );
 };
--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,9 +1,25 @@
-
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+
+    const { currentUser } = useSelector(state => state.user)
+
     return (
-        <div>
-            Profile
+        <div className="w-[40%] mx-auto text-center">
+            <h1 className="text-[2vw]">Profile</h1>
+            <img src={currentUser.profilePicture} alt="" className="mx-auto rounded-full mt-4" />
+
+            <form action="" method="post" className=" mt-5 flex flex-col gap-4 w-[100%] mx-auto">
+                <input type="text" id="username" className="outline-none text-slate-700 bg-slate-300 p-3 rounded-md" />
+                <input type="email" id="email" className="outline-none text-slate-700 bg-slate-300 p-3 rounded-md" />
+                <input type="password" id="password" className="outline-none text-slate-700 bg-slate-300 p-3 rounded-md" />
+                <button className="outline-none bg-slate-700 p-3 rounded-lg text-white uppercase font-[1vw]">Update</button>
+            </form>
+
+            <div className="flex justify-between mt-5 ">
+                <span className="font-[1vw] text-red-500">Delete Account</span>
+                <span className="font-[1vw] text-red-500">Sign out</span>
+            </div>
         </div>
     );
 };
